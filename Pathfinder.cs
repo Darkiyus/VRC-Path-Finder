a982f14a8004db7153aa3fdfa995e46821fc785f
Darkiyus <34716496+Darkiyus@users.noreply.github.com>
2022-02-19 12:29:59 +0100

    Update Pathfinder.cs

--- Pathfinder.cs
+++ Pathfinder.cs
@@ -3,22 +3,14 @@ namespace VRCPathFinder
 {
     public class VRCPath
     {
-
         public static string Search()
         {
             string result = "";
             object steamInstallPath = Registry.CurrentUser.OpenSubKey(@"Software\VRChat").GetValue("");
             result = steamInstallPath.ToString();
-
-
+            
             return result;
-
-
-
         }
-
-
-
 
     }
 }
