3e0a984a7e2e360d4e3571f0a4e78a19d712ed06
Darkiyus <34716496+Darkiyus@users.noreply.github.com>
2022-02-19 02:54:56 +0100

    Add files via upload

---
+++ Pathfinder.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Win32;
+namespace VRCPathFinder
+{
+    public class VRCPath
+    {
+
+        public static string Search()
+        {
+            string result = "";
+            object steamInstallPath = Registry.CurrentUser.OpenSubKey(@"Software\VRChat").GetValue("");
+            result = steamInstallPath.ToString();
+
+
+            return result;
+
+
+
+        }
+
+
+
+
+    }
+}
